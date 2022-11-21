4e9a30b3877e5c8b0d9c221ca16dd467e57c04d0
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2022-11-21 21:11:26 +0900

    add limit-line-css

---
+++ limit-line-css/script.js
@@ -0,0 +1,16 @@
+const num = "100";
+
+switch (num) {
+  case 100:
+    console.log("OK");
+    break;
+  default:
+    console.log("NG");
+    break;
+}
+
+if (num === 100) {
+  console.log("OK");
+} else {
+  console.log("NG");
+}

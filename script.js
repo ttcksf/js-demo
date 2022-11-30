7a4e4679e94c303e8e735a327ae371e7fe9d4eda
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2022-11-30 14:09:07 +0900

    add style-log

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-const text = 0;
+function handleNumber(num) {
+  //左辺と右辺のどちらかがtrueかどうか？
+  //左辺がtrueの時は左辺を返し、それ以外は右辺を返す。
+  // number = num || "エラーです";
 
+  //左辺がnull,undifinedの時は右辺を返し、それ以外は左辺を返す
+  // number = num ?? "エラーです";
 
+  //左辺と右辺の両方の条件が合うとき
+  //左辺がtrueの時に右辺を返す
+  number = (num = 1) && "1です";
 
-if(text === "" || text === null){
-  console.log("NG");
-}else{
-  console.log("OK");
+  console.log(number);
 }
+
+handleNumber(2);
---
+++ style-log/script.js
@@ -0,0 +1,7 @@
+const value = 100;
+
+console.log(`%c値は %c${value}`, "color: red; font-weight: bold;", "");
+
+//スタイルを何かしら当てるものに「%c」を先頭につける（スタイルなしでも）
+//cssは%cの順番に沿って書く
+//cssは複数OK

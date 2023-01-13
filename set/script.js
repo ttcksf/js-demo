29c5e1a711d355c682e4016a71e72c777f4161fc
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2023-01-13 16:32:09 +0900

    add aspect-ratio

--- set/script.js
+++ set/script.js
@@ -1,12 +1,12 @@
-const number = [1, 2, 2, 3, 3, 4];
+const number = ["a", "b", "b", "c", "c", "d"];
 
-// const set = new Set(number);
-// console.log(set);
+const set = new Set(number);
+console.log(set);
 
 const result = number.filter((val, i, arr) => {
   console.log("val:", val);
   console.log("i:", i);
-  // return arr.indexOf(val) = i;
-  return arr.indexOf(val) != i;
+  return arr.indexOf(val) == i;
+  // return arr.indexOf(val) != i;
 });
 console.log(result);
--- wrong-object-freeze/script.js
+++ wrong-object-freeze/script.js
@@ -7,6 +7,18 @@
 // student.id = "0002";
 // console.log(student.id);
 
+const student = Object.freeze({
+  id: "0001",
+  name: "山田",
+  age: 18,
+  address: {
+    preference: "Tokyo",
+    block: "Akabane",
+  },
+});
+// student.address.block = "Seatgaya";
+// console.log(student.address.block);
+
 // const student = Object.freeze({
 //   id: "0001",
 //   name: "山田",
@@ -15,19 +27,7 @@
 //     preference: "Tokyo",
 //     block: "Akabane",
 //   }),
+//   circles: Object.freeze(["football", "tennis"]),
 // });
-// student.address.block = "Seatgaya";
-// console.log(student.address.block);
-
-const student = Object.freeze({
-  id: "0001",
-  name: "山田",
-  age: 18,
-  address: Object.freeze({
-    preference: "Tokyo",
-    block: "Akabane",
-  }),
-  circles: Object.freeze(["football", "tennis"]),
-});
-student.circles.push("baseball");
-console.log(student.circles);
+// student.circles.push("baseball");
+// console.log(student.circles);

de789caa4c92fbfb85dfd988db198b6b0e6f8d7a
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2023-02-08 04:01:04 +0900

    add wrong-function

---
+++ wrong-function/script.js
@@ -0,0 +1,16 @@
+// function test(str) {
+//   console.log(str);
+// }
+// test("hello");
+// console.log(test);
+// console.dir(test);
+// 関数自体はオブジェクトだからconsole.logで関数の名前を指定しても意味がない
+// const data = {
+//   id: "0001",
+//   name: "AAA",
+//   age: 20,
+// };
+// console.log(data);
+// console.dir(data);
+// 関数はオブジェクトのように操作できる
+// test.XXX = "testtesttest";

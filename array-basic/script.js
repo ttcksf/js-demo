da6dc3af1290cc5e60f271eb9f771fc21c0515be
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2023-02-07 18:19:15 +0900

    array-basic

---
+++ array-basic/script.js
@@ -0,0 +1,27 @@
+// const array = [];
+const array = ["A", "B", "C", "D"];
+array.push("E");
+array.push("F");
+array.push([1, 2]);
+array[0] = ["a", "b"];
+console.log(array);
+console.log(array[0]);
+console.log(array.length);
+console.log(array[6][0]);
+
+// 配列とオブジェクトの違いは連番で管理するか、名前で判定するか
+const obj = {
+  id: "0001",
+  name: "aaa",
+  age: 20,
+};
+obj.id = "0002";
+obj.email = "test@test.jp";
+obj.sports = ["baseball", "soccer"];
+
+console.log(obj);
+console.log(obj.id);
+
+const data = ["a", "b", { id: "0001", name: "aaa", age: 20 }];
+console.log(data);
+console.log(data[2].id);

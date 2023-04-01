4e69d2136144d7f480c596455c6dd3cb2b7c05d4
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2023-04-02 01:38:48 +0900

    add wrong-async-function

---
+++ wrong-async-function/script.js
@@ -0,0 +1,17 @@
+// 実行する関数が一つの場合はそのまま書いてOK
+// fetch("https://jsonplaceholder.typicode.com/todos")
+//   .then((response) => response.json())
+//   .then((json) => console.log(json));
+
+// {}は2つ以上の関数を実行することを想定しているためreturnで戻り値を指定する必要がある
+// fetch("https://jsonplaceholder.typicode.com/todos")
+//   .then((response) => {
+//     response.json();
+//   })
+//   .then((json) => console.log(json));
+
+fetch("https://jsonplaceholder.typicode.com/todos")
+  .then((response) => {
+    return response.json();
+  })
+  .then((json) => console.log(json));

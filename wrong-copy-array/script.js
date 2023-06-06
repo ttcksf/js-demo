e5615a643303d4175230341655bff2682310129b
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2023-06-06 14:00:24 +0900

    add wrong-copy-arrat

--- wrong-copy-array/script.js
+++ wrong-copy-array/script.js
@@ -1,34 +1,28 @@
-let obj1 = {
-  id: "0001",
-  name: "aaa",
-  age: 20,
-  sports: {
-    sports1: "baseball",
-    sports2: "soccer",
-  },
-};
-// obj2の中身が変わるとコピー元のobj1が影響を受ける
-// let obj2 = obj1;
-// スプレッド構文で対策できるが階層が深くなるとダメ
-// let obj2 = { ...obj1 };
-
-// コピー元を崩したくない時
-// obj1を一度文字列に変換して、その文字列を元にオブジェクトを新規作成している
-let obj2 = JSON.parse(JSON.stringify(obj1));
-
-obj2.id = "0002";
-obj2.sports.sports1 = "tennis";
 let arr1 = [1, 2, 3, [4, 5, 6]];
 
 // let arr2 = arr1;
-let arr2 = [...arr1];
+// let arr2 = [...arr1];
+let arr2 = copyArray(arr1);
 
 arr2[0] = 4;
 arr2[3][0] = 5;
 
+function copyArray(arr) {
+  let copy = [];
+  for (let i = 0; i < arr.length; i++) {
+    // インデックス番号ごとに代入するが、中に配列があるとダメ
+    // copy[i] = arr[i];
+
+    // 値が配列の時
+    if (Array.isArray(arr[i])) {
+      copy[i] = copyArray(arr[i]);
+    } else {
+      copy[i] = arr[i];
+    }
+  }
+  return copy;
+}
 const container = document.querySelector(".container");
 const wrapper = document.querySelector(".wrapper");
-// container.textContent = JSON.stringify(obj1);
-// wrapper.textContent = JSON.stringify(obj2);
 container.textContent = JSON.stringify(arr1);
 wrapper.textContent = JSON.stringify(arr2);

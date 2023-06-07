a8020acb66ca999706cc4200b2a21c49660e8551
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2023-06-07 15:20:30 +0900

    fix:wrong-copy-array-advance

--- wrong-copy-array-advance/script.js
+++ wrong-copy-array-advance/script.js
@@ -20,21 +20,28 @@ arr2[3].age = 24;
 function copyArray(arr) {
   let copy = [];
   for (let i = 0; i < arr.length; i++) {
-    // インデックス番号ごとに代入するが、中に配列があるとダメ
+    // インデックス番号ごとに代入するが、中に配列,オブジェクトがあるとダメ
     // copy[i] = arr[i];
+    // [] = [1, 2, 3, [4, 5, 6]]
+    // [1] = [1, 2, 3, [4, 5, 6]]
+    // [1,2] = [1, 2, 3, [4, 5, 6]]
 
     // 値が配列か
     if (Array.isArray(arr[i])) {
+      console.count(`インデックス番号${i}番目はcopyArrayのif通ったよ`);
       // 自分自身をもう一度呼び出してelse文に辿り着かせる
       copy[i] = copyArray(arr[i]);
       // 値がオブジェクトか
     } else if (typeof arr[i] === "object" && arr[i] !== null) {
+      console.count(`インデックス番号${i}番目はcopyArrayのelseif通ったよ`);
       // copyObjに移動する
       copy[i] = copyObj(arr[i]);
       // 通常の処理
     } else {
+      console.count(`インデックス番号${i}番目はcopyArrayのelse通ったよ`);
       copy[i] = arr[i];
     }
+    console.log(copy);
   }
   return copy;
 }
@@ -44,14 +51,17 @@ function copyObj(obj) {
   for (let key in obj) {
     // 値が配列か
     if (Array.isArray(obj[key])) {
+      console.count(`インデックス番号${key}番目はcopyObjのif通ったよ`);
       // copyArrayに移動する
       copy[key] = copyArray(obj[key]);
       // 値がオブジェクトか
     } else if (typeof obj[key] === "object" && obj[key] !== null) {
+      console.count(`インデックス番号${key}番目はcopyObjのelseif通ったよ`);
       // 自分自身をもう一度呼び出してelse文に辿り着かせる
       copy[key] = copyObj(obj[key]);
       // 通常の処理
     } else {
+      console.count(`オブジェクトの${key}はcopyObjのelse通ったよ`);
       copy[key] = obj[key];
     }
   }

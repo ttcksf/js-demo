751fdb41ea9a72cd024fdb2dd9a8fc8f837e1ed8
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2023-06-07 15:07:53 +0900

    fix:wrong-copy-array

--- wrong-copy-array/script.js
+++ wrong-copy-array/script.js
@@ -12,13 +12,19 @@ function copyArray(arr) {
   for (let i = 0; i < arr.length; i++) {
     // インデックス番号ごとに代入するが、中に配列があるとダメ
     // copy[i] = arr[i];
+    // [] = [1, 2, 3, [4, 5, 6]]
+    // [1] = [1, 2, 3, [4, 5, 6]]
+    // [1,2] = [1, 2, 3, [4, 5, 6]]
 
     // 値が配列の時
     if (Array.isArray(arr[i])) {
+      console.count(`インデックス番号${i}番目はif通ったよ`);
       copy[i] = copyArray(arr[i]);
     } else {
+      console.count(`インデックス番号${i}番目はelse通ったよ`);
       copy[i] = arr[i];
     }
+    console.log(copy);
   }
   return copy;
 }

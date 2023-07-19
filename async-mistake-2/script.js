879693253554aec617d1d4c9466f9364f99c76ba
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2023-07-19 17:01:18 +0900

    fix:async-mistake-2

--- async-mistake-2/script.js
+++ async-mistake-2/script.js
@@ -1,19 +1,17 @@
-const fetchData1 = async (count) => {
+const fetchData = async (count) => {
   const array = [];
-  const res = await fetch(
-    `https://jsonplaceholder.typicode.com/todos/${count}`
-  );
-  const data = await res.json();
+
   for (let i = 1; i <= count; i++) {
-    array.push(data.title);
+    array.push(
+      fetch(`https://jsonplaceholder.typicode.com/todos/${i}`).then((res) =>
+        res.json()
+      )
+    );
   }
-  // 通常のPromiseはfetchが終わってからforに入る
-  // Promise.allとするとfetchとforを非同期（同時に）実行する
-  const newArray = await Promise.all(array);
-  // console.log(data.title);
-  // console.log(array);
-  console.log(newArray);
-  // return array;
-  return newArray;
+  const data = await Promise.all(array);
+  console.log(data);
 };
-fetchData1(30);
+fetchData(100);
+
+// 通常のPromiseはfetchが終わってからforに入る
+// Promise.allとするとfetchとforを非同期（同時に）実行する

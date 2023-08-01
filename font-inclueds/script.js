7399c950d8275131d0fe5fda326f1de488ee102f
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2023-08-02 02:32:44 +0900

    fix:font-includes

--- font-inclueds/script.js
+++ font-inclueds/script.js
@@ -1,12 +1,15 @@
 const arr = ["Web制作", "Webマーケ", "動画編集"];
+const str = "Web制作";
 
 // let result1 = arr.includes("Web制作");
 // 第二引数で配列内の場所まで検証できる
 let result1 = arr.includes("Web制作", 1);
 // 完全一致であることまで検証する
 // let result1 = arr.includes("web制作");
+// テキストから文字の有無も確認できる
+let result2 = str.includes("Web");
 
 const container = document.querySelector(".container");
 container.innerHTML = result1;
 const wrapper = document.querySelector(".wrapper");
-wrapper.innerHTML = text1;
+wrapper.innerHTML = result2;

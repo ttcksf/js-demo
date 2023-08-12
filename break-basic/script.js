88fcc49f10270a61aef3645b0042cde976873325
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2023-08-12 20:57:20 +0900

    break-basic

---
+++ break-basic/script.js
@@ -0,0 +1,16 @@
+const arr = ["a", "a", "b", "a", "a", "a"];
+let data;
+for (let i = 0; i < arr.length; i++) {
+  console.count(i);
+  if (arr[i] === "b") {
+    data = "bを見つけた！";
+    // breakは見つけた時にループを終了させる。
+    // breakを使わない場合は総当たりして終わる
+    break;
+  }
+}
+
+const container = document.querySelector(".container");
+const wrapper = document.querySelector(".wrapper");
+container.textContent = data;
+wrapper.textContent = "bbb";

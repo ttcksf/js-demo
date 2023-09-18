ae4d921c7a1c19e8d4e07adf69f2dc7e44df3b20
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2023-09-18 14:12:44 +0900

    add window-vs-document

---
+++ window-vs-document/script.js
@@ -0,0 +1,18 @@
+// window.addEventListener("load", () => {
+//   console.log("window");
+// });
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   console.log("document");
+// });
+
+// windowは画面、documentはDOMツリー
+// iframeの中でもdocumentがあることがわかる
+console.log(window);
+console.log(document);
+
+console.dir(window);
+console.dir(document);
+
+// windowの中にdocumentがあるのでこの書き方でもOK
+// console.log(window.document);

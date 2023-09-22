e348025a8d4863f25182b65172dc94022e64d2a8
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2023-09-22 13:15:17 +0900

    fix:circle-bg

--- circle-animation/script.js
+++ circle-animation/script.js
@@ -1,4 +1,10 @@
 const mask = document.querySelector(".mask");
+const circleItem1 = document.querySelector(".circle-item1");
+const circleItem2 = document.querySelector(".circle-item2");
+const circleItem3 = document.querySelector(".circle-item3");
+const circleItem4 = document.querySelector(".circle-item4");
+const circleItem5 = document.querySelector(".circle-item5");
+const circleItem6 = document.querySelector(".circle-item6");
 
 //角度の初期状態(0°)
 let angle = 0;
@@ -18,5 +24,23 @@ window.addEventListener("scroll", () => {
   if (window.scrollY > mask.offsetTop + 100) {
     //アニメーションを開始
     setInterval(drawCircle, 100);
+    setTimeout(() => {
+      circleItem1.classList.add("anime");
+    }, 200);
+    setTimeout(() => {
+      circleItem2.classList.add("anime");
+    }, 300);
+    setTimeout(() => {
+      circleItem3.classList.add("anime");
+    }, 600);
+    setTimeout(() => {
+      circleItem4.classList.add("anime");
+    }, 900);
+    setTimeout(() => {
+      circleItem5.classList.add("anime");
+    }, 1100);
+    setTimeout(() => {
+      circleItem6.classList.add("anime");
+    }, 1400);
   }
 });

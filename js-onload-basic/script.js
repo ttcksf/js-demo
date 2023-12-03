da298d6fd767b87a294829fd9d8243d8077923d2
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2023-12-03 12:03:56 +0900

    fix:js-scroll-event

--- js-onload-basic/script.js
+++ js-onload-basic/script.js
@@ -1,21 +1,20 @@
 const container = document.querySelector('.container');
 window.addEventListener('scroll', () => {
-  // 方向は関係なくスクロールしたことを検知する
-  // console.count('スクロールした！');
-  // スクロール量もわかる
+  // スクロール量
   const scrollVal = window.scrollY;
-  // console.log(scrollVal);
+  console.log('scrollVal:', scrollVal);
   // １回のスクロールで表示できる画面の高さ（スクロールを重ねることでHTML要素の高さぶんを表示できることになる）
   const windowH = window.innerHeight;
+  console.log('windowH:', windowH);
   // 指定したHTML要素の高さ
   const containerH = container.scrollHeight;
-  // console.log(windowH, containerH);
-
-  // スクロールし切れていない残りの量
+  console.log('containerH:', containerH);
+  // スクロールし切れていない残りの量(実務では余裕を持たせる)
   const scrollable = containerH - windowH - 100;
+  console.log('scrollable:', scrollable);
+
   // 全て読んだらという条件式
   if (Math.ceil(scrollVal) >= Math.ceil(scrollable)) {
-    const agree = document.querySelector('.agree');
-    agree.style.opacity = 1;
+    confirm('同意しますか？');
   }
 });

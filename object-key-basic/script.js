106fab69a164044e623ab39466aad7a36b577bff
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-01-01 14:26:41 +0900

    add object-keys-basic

---
+++ object-key-basic/script.js
@@ -0,0 +1,27 @@
+const text1 = document.querySelectorAll('.text');
+// NodeList
+console.log(text1);
+text1.forEach((txt) => {
+  console.log(txt.textContent);
+});
+
+const text2 = document.getElementsByClassName('text');
+// HTMLCollectionは配列風オブジェクトでmapやforEachのような配列に使うものは使えない
+console.log(text2);
+// text2.forEach((txt) => {
+//   console.log(txt.textContent);
+// });
+// for文はいろんなデータに使用できる
+for (const txt of text2) {
+  console.log(txt.textContent);
+}
+// 配列に変換すれば使えるようになる
+Array.from(text2).forEach((txt) => {
+  console.log(txt.textContent);
+});
+// 配列にあるインデックス番号のようなキーを作る
+const keys = Object.keys(text2);
+console.log(keys);
+keys.forEach((key) => {
+  console.log(text2[key].textContent);
+});

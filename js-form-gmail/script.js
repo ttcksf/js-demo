b6a866dccdb1fbeb2cb976c59a0e813b7b70a579
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-03-15 10:46:08 +0900

    fix2:js-form-gmail

--- js-form-gmail/script.js
+++ js-form-gmail/script.js
@@ -6,8 +6,35 @@ const subject = document.querySelector('.subject');
 const text = document.querySelector('.text');
 
 function sendEmail() {
-  console.log('送信');
+  // console.log('送信');
+  const bodyMsg = `
+    お名前：${fname.value}<br>
+    メールアドレス：${email.value}<br>
+    電話番号：${tel.value}<br>
+    件名：${subject.value}<br>
+    内容：${text.value}
+  `;
+  Email.send({
+    SecureToken: '01b2bd5f-ed12-49bf-a55b-23b6c7f6bcd4',
+    To: 'ttc0104ksf1993@gmail.com',
+    From: 'ttc0104ksf1993@gmail.com',
+    // Subject: 'テスト送信',
+    Subject: subject.value,
+    // Body: 'こちらはテストメールです',
+    Body: bodyMsg,
+  }).then((message) => alert(message));
 }
+// setup an smtp server hereをクリックして右上のログインボタンからログイン
+// アンケートは一番上を回答して終了する
+// settings→create SMTPでGmailアドレスを入力してcreateボタンをクリック
+// hostとusernameとpasswordをポップアップの内容に差し替えてポップアップを閉じる
+// settings→manage domains→Email verification→Start verification
+// I don’t own a domain→verify email→アドレスを入力→メール検証をクリアしてリロード
+// Email verificationでメールアドレスがverifiedになる
+
+// Encrypt your smtp credentialsをクリックして、UserNameとPasswordをコピペして、GenerateToken
+// 表示されたトークンをSecureTokenの値にコピペ
+// ↑からHostとUsernameとPasswordを削除する
 
 function checkInput() {
   const inputList = document.querySelectorAll('.input');
@@ -45,6 +72,10 @@ function checkInput() {
 
 form.addEventListener('submit', (e) => {
   checkInput();
-  e.preventDefault();
-  sendEmail();
+  // e.preventDefault();
+  // sendEmail();
+  if (form.querySelectorAll('.error').length === 0) {
+    e.preventDefault();
+    sendEmail();
+  }
 });
